b495ffce81b0c73a7bbf8b322b09e30f51a43e82
Levin <223221485+levin1337@users.noreply.github.com>
2026-01-06 20:52:17 +0300

    first commit

---
+++ StartGhostSense.bat
@@ -0,0 +1,3 @@
+color 7
+"jdk\bin\java.exe" -Xmx4096M -Djava.library.path="natives" -cp "libraries\*";"libraries\fabric-loader-0.18.2.jar" net.fabricmc.loader.impl.launch.knot.KnotClient --gameDir "game" --assetsDir "game\assets" --assetIndex "19" --version "1.21.4"
+pause
